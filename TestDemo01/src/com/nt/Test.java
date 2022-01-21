e9b1b4abaac7b68dd977cf61f7c66d750963ebf0
nawaj.a <nawaj.a@VLSYS52-23.virinchi.com>
2022-01-21 12:40:26 +0530

    m

--- TestDemo01/src/com/nt/Test.java
+++ TestDemo01/src/com/nt/Test.java
@@ -5,13 +5,9 @@ public class Test {
 	private int id;
 	private int fee;
 	
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-=======
-	public void m1() {
-		System.out.println("m1");
->>>>>>> refs/heads/Branch01_OTPUPI
 	}
+
 }
