894010101041713f17e5d2af316607f53c4c5bfe
nawaj.a nawaj.a@VLSYS52-23.virinchi.com <nawaj.a nawaj.a@VLSYS52-23.virinchi.com>
2022-02-23 15:09:19 +0530

    uri

---
+++ SmartContactManager/src/main/java/com/nt/controller/UriConstant.java
@@ -0,0 +1,17 @@
+package com.nt.controller;
+
+public class UriConstant {
+
+	public static final String ABOUT_URL="about";
+	public static final String SIGNUP="signup";
+	public static final String SIGNIN="signin";
+	public static final String INDEX="index";
+	public static final String ADD="add";
+	public static final String SHOW="show";
+	public static final String CONTACT="contact";
+	public static final String PROFILE="profile";
+	public static final String UPDATE_PHOTO="updatePhoto";
+	public static final String DELETE="delete";
+	public static final String EDIT="edit";
+	public static final String UPDATE="update";
+}
